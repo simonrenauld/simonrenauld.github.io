b0330dffb7b002baeabd5f3969cb79420179195e
simonrenauld <simon.renauld@pharmapoc.com>
2021-07-11 09:48:18 +0700

    commit on main

--- js/main.js
+++ js/main.js
@@ -285,6 +285,8 @@
 		})
 	};
 
+	
+
 	// Document on load.
 	$(function(){
 		fullHeight();
@@ -305,5 +307,16 @@
 		owlCrouselFeatureSlide();
 	});
 
+	function openNav() {
+		document.getElementById("mySidenav").style.width = "250px";
+		document.getElementById("main").style.marginLeft = "250px";
+		document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	  }
+	  
+	  function closeNav() {
+		document.getElementById("mySidenav").style.width = "0";
+		document.getElementById("main").style.marginLeft= "0";
+		document.body.style.backgroundColor = "white";
+	  }	
 
 }());
