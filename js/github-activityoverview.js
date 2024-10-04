e488c69116d06d4195a502d871f6fa06ff95b9e7
simonrenauld <simonrenauld.data@gmail.com>
2024-10-04 10:11:05 +0700

    activity

--- js/github-activityoverview.js
+++ js/github-activityoverview.js
@@ -1,30 +1,37 @@
 const githubUsername = 'simonrenauld';
-
 const activityOverview = document.querySelector('.activity-overview');
 const activityCountElement = document.getElementById('activity-count');
 const repositoryCountElement = document.getElementById('repository-count');
 const repositoryList = document.querySelector('.repository-list');
 
 function fetchActivityData() {
-  fetch(`/api/github-activity?username=${githubUsername}`)
-  .then(response => response.json())
-  .then(data => {
-    const activityCount = data.length;
-    const repositoryCount = new Set(data.map(event => event.repo.name)).size;
-    
-    activityCountElement.textContent = `Created ${activityCount} events`;
-    repositoryCountElement.textContent = `in ${repositoryCount} repositories`;
-    
-    repositoryList.innerHTML = '';
-    data.forEach(event => {
-      const repositoryName = event.repo.name;
-      const commitCount = event.payload.commits ? event.payload.commits.length : 0;
-      repositoryList.innerHTML += `<li><a href="https://github.com/${repositoryName}">${repositoryName} (${commitCount} commits)</a></li>`;
+  fetch(`https://api.github.com/users/${githubUsername}/events?per_page=30`)
+    .then(response => response.json())
+    .then(data => {
+      const activityCount = data.length;
+      const repositoryCount = new Set(data.map(event => event.repo.name)).size;
+      
+      if (activityCountElement) {
+        activityCountElement.textContent = `Created ${activityCount} events`;
+      }
+      if (repositoryCountElement) {
+        repositoryCountElement.textContent = `in ${repositoryCount} repositories`;
+      }
+      
+      if (repositoryList) {
+        repositoryList.innerHTML = '';
+        data.forEach(event => {
+          if (event.type === 'PushEvent') {
+            const repositoryName = event.repo.name;
+            const commitCount = event.payload.commits ? event.payload.commits.length : 0;
+            repositoryList.innerHTML += `<li><a href="https://github.com/${repositoryName}" target="_blank">${repositoryName} (${commitCount} commits)</a></li>`;
+          }
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching activity data:', error);
     });
-  })
-  .catch(error => {
-    console.error('Error fetching activity data:', error);
-  });
 }
 
 fetchActivityData();
